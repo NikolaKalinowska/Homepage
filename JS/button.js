1cb0f615fdb768d9f7bb38fdd903a7929949393d
Nikola Kalinowska <nikola.kalinowska1999@gmail.com>
2023-10-09 16:10:25 +0200

    Added funtions to button.js

--- JS/button.js
+++ JS/button.js
@@ -1,16 +1,24 @@
-console.log("Hejka!")
-
-let button = document.querySelector(".button");
-let body = document.querySelector(".background");
-let themeName = document.querySelector(".themeName");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    if (body.classList.contains("dark")) {
-        themeName.innerText = "JASNY";
-    } else {
-        themeName.innerText = "CIEMNY";
+{
+    const welcome() => {
+        console.log("Hejka!")
     }
-});
 
+    const onChangeBackgroundClick = () => {
+        body.classList.toggle("dark");
+        if (body.classList.contains("dark")) {
+            themeName.innerText = "JASNY";
+        } else {
+            themeName.innerText = "CIEMNY";
+        }
+    };
+    const init = () => {
+        const button = document.querySelector(".button");
+        const body = document.querySelector(".background");
+        const themeName = document.querySelector(".themeName");
 
+        button.addEventListener("click", onChangeBackgroundClick);
+        welcome();
+    };
+
+    init();
+}
