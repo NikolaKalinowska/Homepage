f9627c6928f24be88b8911e5a0a330b141c8665f
Nikola Kalinowska <nikola.kalinowska1999@gmail.com>
2023-10-09 12:48:55 +0200

    Added button for background color change

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,14 @@
-let scrollToFooter = () => {
-    const footer = document.getElementById('footer');
-    footer.scrollIntoView({ behavior: 'smooth' });
-};
+console.log("Hejka!")
 
-document.getElementById('scroll-to-footer').addEventListener('click', scrollToFooter); 
+let button = document.querySelector(".button");
+let body = document.querySelector(".background");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+    if (body.classList.contains("dark")) {
+        themeName.innerText = "JASNY";
+    } else {
+        themeName.innerText = "CIEMNY";
+    }
+});
